ce7020428bdea20d196d16ba0fe586edebc17c55
Rizza Marwan <rizzamarwan@icloud.com>
2024-12-07 23:07:08 +0700

    Update

--- index.js
+++ index.js
@@ -1,43 +1,79 @@
 import { menuArray } from './data.js'
 
-document.addEventListener('click', function(e) {
-    if (e.target.dataset.addMenu) {
-        handleAddMenu(e.target.dataset.addMenu)
-    }
-})
+// const menuList = document.getElementById('menu')
 
-function handleAddMenu(menuId) {
-    const targetAddMenu = menuArray.filter(function(menu) {
-        return menu.id === menuId
-    })
-    render()
-}
-
-function restaurantMenu() {
-    let dinerMenu = ''
-
-    menuArray.map(function(menu) {
-        dinerMenu += `
+const renderMenu = function(dinerMenu) {
+    return dinerMenu.map(function({emoji, name, ingredients, price, id}) {
+        return `
             <div class="menu-list" id="menu-list">
-                    <p class="menu-image">${menu.emoji}</p>
+                    <p class="menu-image">${emoji}</p>
                     <div class="menu-details">
-                        <h1>${menu.name}</h1>
-                        <p>${menu.ingredients.join(', ')}</p>
-                        <h2>$${menu.price}</h2>
+                        <h1>${name}</h1>
+                        <p>${ingredients.join(', ')}</p>
+                        <h2>$${price}</h2>
                     </div>
-                <button class="add-btn" id="add-btn" data-addmenu="${menu.id}">+</button>
+                <button class="add-btn" id="add-btn" data-addmenu="${id}">+</button>
             </div>
         `
     }).join('')
-    return dinerMenu
 }
 
-function restaurantCart() {
+document.getElementById('menu').innerHTML = renderMenu(menuArray)
 
+let dinerCart = []
+
+if (dinerCart.length <= 0) {
+    document.getElementById('diner-cart').classList.add('hidden')
 }
 
-function render() {
-    document.getElementById('feed').innerHTML = restaurantMenu()
+const renderCart = function(dinerCart) {
+    return dinerCart.map(function({name, id, count, price}) {
+        return `
+            <div class="cart-item" id="cart-item">
+                <h1>${name}</h1><span data-remove="${id}">remove</span>
+                <h2>${count} X $${price} = $${count * price}</h2>
+            </div>
+        `
+    }).join('')
 }
 
-render()
+const handleAddItem = function(itemId) {
+    const targetMenuObj = menuArray.find(function(item) {
+        item.id === Number(itemId)
+    })
+
+    const existingItem = dinerCart.find(function(item) {
+        item.id === targetMenuObj.id
+    })
+
+    if (existingItem) {
+        existingItem.count += 1
+    } else {
+        dinerCart.push({...targetMenuObj, count: 1})
+    }
+
+    document.getElementById('diner-cart').classList.remove('hidden')
+}
+
+// document.addEventListener('click', function(e) {
+//     if (e.target.dataset.addMenu) {
+//         handleAddMenu(e.target.dataset.addMenu)
+//     }
+// })
+
+// function handleAddMenu(menuId) {
+//     const targetAddMenu = menuArray.filter(function(menu) {
+//         return menu.id === menuId
+//     })
+//     render()
+// }
+
+// function restaurantCart() {
+
+// }
+
+// function render() {
+//     document.getElementById('menu').innerHTML = restaurantMenu
+// }
+
+// render()
